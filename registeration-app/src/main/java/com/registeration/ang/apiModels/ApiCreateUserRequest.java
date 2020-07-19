397cb594fb13101b97fd7e1971ced2a5db90c66a
mohamed <m.ibrahem@tap.company>
2020-07-19 17:10:55 +0200

    removing the confirm password validation  using annotation

--- registeration-app/src/main/java/com/registeration/ang/apiModels/ApiCreateUserRequest.java
+++ registeration-app/src/main/java/com/registeration/ang/apiModels/ApiCreateUserRequest.java
@@ -2,13 +2,9 @@ package com.registeration.ang.apiModels;
 
 import javax.validation.constraints.NotEmpty;
 
-import com.registeration.ang.annotations.FieldMatch;
-
 import lombok.Data;
 
 @Data
-@FieldMatch.List({
-		@FieldMatch(first = "password", second = "confirmPassword", message = "confirm password must match the password") })
 public class ApiCreateUserRequest {
 
 	@NotEmpty(message = "Username is required")
--- registeration-app/src/main/java/com/registeration/ang/dto/UserDto.java
+++ registeration-app/src/main/java/com/registeration/ang/dto/UserDto.java
@@ -2,7 +2,6 @@ package com.registeration.ang.dto;
 
 import java.io.Serializable;
 
-import com.registeration.ang.annotations.FieldMatch;
 import com.registeration.ang.apiModels.ApiBaseModel;
 
 import lombok.Data;
@@ -10,8 +9,6 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
-@FieldMatch.List({
-		@FieldMatch(first = "password", second = "confirmPassword", message = "The password fields must match") })
 public class UserDto extends ApiBaseModel implements Serializable {
 
 	private static final long serialVersionUID = 1L;
